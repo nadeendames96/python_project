0ea7e9c24540f7dc454210cb6de95cb72a1ea2d9
Nadeen Dames <nadeen@me.com>
2020-07-27 19:35:02 +0300

    Added Files

---
+++ recipe/recipe.py
@@ -0,0 +1,17 @@
+# Recipe
+
+ingredients = ["2 avocados", "1 lime", "2 tsp salt"]
+instructions = ["chop avocados", "chop onion",
+                "squeeze lime", "add salt", "mix well"]
+
+
+class Recipe:
+
+    def print_ingredients(self):
+        pass
+
+    def print_instructions(self):
+        pass
+
+    def add_ingredient(self, ingredient):
+        pass
