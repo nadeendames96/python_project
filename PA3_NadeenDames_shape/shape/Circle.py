c357255da04c081b6b036f5aa685317359b9c4d7
Nadeen Dames <nadeen@me.com>
2020-07-27 17:14:17 +0300

    Edit file Circle.py

---
+++ PA3_NadeenDames_shape/shape/Circle.py
@@ -0,0 +1,19 @@
+
+# Circle class
+
+
+class Circle(Shape):
+    pass
+    # TODO: Implement __init__() for this class
+
+    def __init__(self, radius=1.0):
+        # Remember this is a subclass of Shape!
+        self.radius = radius
+
+    # TODO: Implement find_area() for this class
+    def find_area(self):
+        return 3.14 * self.radius**2
+
+    # TODO: Implement find_circumference() for this class
+    def find_circumference(self):
+        return 2 * 3.14 * self.radius
