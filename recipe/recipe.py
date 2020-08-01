721a51d5ed77e0c615624bae5447e1bbe56938f3
Nadeen Dames <nadeen@me.com>
2020-08-01 15:11:00 +0300

    Modified File

--- recipe/recipe.py
+++ recipe/recipe.py
@@ -3,38 +3,58 @@
 
 
 class Recipe:
+    list_of_ingredients=[]
+    list_of_instructions=[]    
+    name=""
+    preparation_time=0
 
-    def __init__(self):
-        self.ingredients = ["2 avocados", "1 lime", "2 tsp salt"]
-        self.instructions = ["chop avocados", "chop onion",
-                "squeeze lime", "add salt", "mix well"]
+    def __init__(self,name,preparation_time):
+        # self.ingredients = ["2 avocados", "1 lime", "2 tsp salt"]
+        # self.instructions = ["chop avocados", "chop onion",
+        #         "squeeze lime", "add salt", "mix well"]
 
-        print("Welcome....!!!!")
-
+        # print("Welcome....!!!!")
+        self.name=name
+        self.preparation_time=preparation_time
         pass
-    def print_ingredients(self):
-        count=0
-        for ingredient in self.ingredients:
-            count+=1
-            print(f"ingredient num {count} is {ingredient}")
-        pass
 
-    def print_instructions(self):
-        count=0
-        for instruction in self.instructions:
-            count+=1
-            print(f"instruction num {count} is {instruction}")
-        pass
+  
 
     def add_ingredient(self, ingredient):
-        print("Successful Adding {ingredient}")
+        self.list_of_ingredients=open("ingredients.txt","a+")
+        self.list_of_ingredients.write(ingredient)
         pass
-    def add_instructions(self, instructions):
-        print("Successful Adding {instructions}")
+    def add_instructions(self, instruction):
+        self.list_of_instructions=open("instructions.txt","a+")
+        self.list_of_instructions.write(instruction)
+        pass
+    def read_ingredients(self):
+        self.list_of_ingredients=open("ingredients.txt","r")    
+        #print(self.list_of_ingredients.read())
+        ingredient=self.list_of_ingredients.readlines()
+        for ingredient_ in ingredient:
+            print(f"\t * {ingredient_}")
+        pass
+
+    def read_instructions(self):
+        self.list_of_instructions=open("instructions.txt","r") 
+        instruction=self.list_of_instructions.readlines()
+        for instruction_ in instruction:
+            print(f"\t * {instruction_}")
+        #print(self.list_of_instructions.read())
+        pass
+    def print_ingredients(self):
+        print(f"To make {self.name} you will need:\n")
+        print(f"{self.read_ingredients()}")
+        
+        
+    def print_instructions(self):
+        print(f"To make {self.name}  you need to do:\n")
+        print(f"{self.read_instructions()}")
 
         pass
-recipe=Recipe()
-print()    
+
+recipe=Recipe("fruite Salat",2)
 recipe.print_ingredients()
-print()
+print("\n")
 recipe.print_instructions()
